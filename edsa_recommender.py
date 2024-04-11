b37f480905b8ba432f36cafd6de685ec35b6f923
Mpho <mokmelive@gmail.com>
2024-04-11 11:50:59 +0200

    feedback page changing
    .

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -27,9 +27,6 @@
 """
 # Streamlit dependencies
 import streamlit as st
-import requests
-import json
-
 # Data handling dependencies
 import pandas as pd
 import numpy as np
@@ -41,19 +38,6 @@ from recommenders.content_based import content_model
 
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
-
-
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    try:
-        data = requests.get(url)
-        data = data.json()
-        poster_path = data['poster_path']
-        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-        return full_path
-    except Exception as e:
-        print(f"Error fetching poster for movie ID {movie_id}: {e}")
-        return "https://via.placeholder.com/500x750?text=No+Poster+Available"
 
 # App declaration
 def main():
@@ -67,114 +51,24 @@ def main():
     # -------------------------------------------------------------------
     page_selection = st.sidebar.selectbox("Choose Option", page_options)
     if page_selection == "Recommender System":
-        st.write('# ByteNest - Movie Recommender Engine')
+        # Header contents
+        st.write('# Movie Recommender Engine')
         st.write('### EXPLORE Data Science Academy Unsupervised Predict')
-        st.image('resources/imgs/Image_header.png', use_column_width=True)
-        
-    source = st.sidebar.checkbox("Data Source")
-    if source:
-            st.subheader("Where did we get the data from?")
-
-            st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/1200px-IMDB_Logo_2016.svg.png", use_column_width=False)
-
-            st.markdown("The data was obtained from the MovieLens \
-            which has the several millions 5-star ratings obtained from users using the online recommendation system.\
-            The IMBD (imbd_df) was legally scraped from IMDB.")
-
-    look = st.sidebar.checkbox("Data")
-    if look:
-            st.subheader("What did the data consists?")
-            st.markdown("* Movies -Movie titles are entered manually or imported from https://www.themoviedb.org/, and include the year of release in parentheses")
-            st.markdown("* Genome_scores - is a score mapping the strength between movies and tag-related properties.")
-            st.markdown("* Genome_tags - User assigned for the movies within the dataset.")
-            st.markdown("* Imdb - Additional movie metadata scraped from IMDB using the links.csv file.")
-            st.markdown("* Links - File providing a mapping between a MovieLens ID and associated IMDB and TMDB IDs.")
-            st.markdown("* Train - The training split of the dataset. Contains user and movie IDs with associated rating data.")    
-        
-    input_method = st.selectbox("Select Input Method", ["Text Input", "Voice Input"])
-    if input_method == "Text Input":
-            st.write('### Enter Your Three Favorite Movies')
-            sys = st.radio("Select an algorithm",
+        st.image('resources/imgs/Image_header.png',use_column_width=True)
+        # Recommender System algorithm selection
+        sys = st.radio("Select an algorithm",
                        ('Content Based Filtering',
                         'Collaborative Based Filtering'))
-            selected_movie1 = st.selectbox("First Option", movie_list)
-            selected_movie2 = st.selectbox("Second Option", movie_list)
-            selected_movie3 = st.selectbox("Third Option", movie_list)
-    elif input_method == "Voice Input":
-            st.write(" Speak Your Three Favorite Movies")
- 
 
-    if st.button("Recommend"):
-            try:
-                with st.spinner("Crunching the numbers..."):
-                    recommended_movie_names1, recommended_movie_posters1 = content_model(movie_list=fav_movies,
-                                                            top_n=10)(selected_movie1)
-                    recommended_movie_names2, recommended_movie_posters2 = content_model(movie_list=fav_movies,
-                                                            top_n=10)(selected_movie2)
-                    recommended_movie_names3, recommended_movie_posters3 = content_model(movie_list=fav_movies,
-                                                            top_n=10)(selected_movie3)
-                st.write("We think you'll like:")
-                col1, col2, col3, col4, col5 = st.columns(5)
-                with col1:
-                    st.text(recommended_movie_names1[0])
-                    st.image(recommended_movie_posters1[0])
-                with col2:
-                    st.text(recommended_movie_names1[1])
-                    st.image(recommended_movie_posters1[1])
-                with col3:
-                    st.text(recommended_movie_names1[2])
-                    st.image(recommended_movie_posters1[2])
-                with col4:
-                    st.text(recommended_movie_names1[3])
-                    st.image(recommended_movie_posters1[3])
-                with col5:
-                    st.text(recommended_movie_names1[4])
-                    st.image(recommended_movie_posters1[4])
-                col6, col7, col8, col9, col10 = st.columns(5)
-                with col6:
-                    st.text(recommended_movie_names2[0])
-                    st.image(recommended_movie_posters2[0])
-                with col7:
-                    st.text(recommended_movie_names2[1])
-                    st.image(recommended_movie_posters2[1])
-                with col8:
-                    st.text(recommended_movie_names2[2])
-                    st.image(recommended_movie_posters2[2])
-                with col9:
-                    st.text(recommended_movie_names2[3])
-                    st.image(recommended_movie_posters2[3])
-                with col10:
-                    st.text(recommended_movie_names2[4])
-                    st.image(recommended_movie_posters2[4])
-                col11, col12, col13, col14, col15 = st.columns(5)
-                with col11:
-                    st.text(recommended_movie_names3[0])
-                    st.image(recommended_movie_posters3[0])
-                with col12:
-                    st.text(recommended_movie_names3[1])
-                    st.image(recommended_movie_posters3[1])
-                with col13:
-                    st.text(recommended_movie_names3[2])
-                    st.image(recommended_movie_posters3[2])
-                with col14:
-                    st.text(recommended_movie_names3[3])
-                    st.image(recommended_movie_posters3[3])
-                with col15:
-                    st.text(recommended_movie_names3[4])
-                    st.image(recommended_movie_posters3[4])
-            except Exception as e:
-                st.write("Oops! Looks like this algorithm doesn't work. We'll need to fix it!")
-                st.write(f"Error: {e}")
-     
         # User-based preferences
-    st.write('### Enter Your Three Favorite Movies')
-    movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
-    movie_2 = st.selectbox('Second Option',title_list[25055:25255])
-    movie_3 = st.selectbox('Third Option',title_list[21100:21200])
-    fav_movies = [movie_1,movie_2,movie_3]
+        st.write('### Enter Your Three Favorite Movies')
+        movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
+        movie_2 = st.selectbox('Second Option',title_list[25055:25255])
+        movie_3 = st.selectbox('Third Option',title_list[21100:21200])
+        fav_movies = [movie_1,movie_2,movie_3]
 
         # Perform top-10 movie recommendation generation
-    if sys == 'Content Based Filtering':
+        if sys == 'Content Based Filtering':
             if st.button("Recommend"):
                 try:
                     with st.spinner('Crunching the numbers...'):
@@ -188,7 +82,7 @@ def main():
                               We'll need to fix it!")
 
 
-    if sys == 'Collaborative Based Filtering':
+        if sys == 'Collaborative Based Filtering':
             if st.button("Recommend"):
                 try:
                     with st.spinner('Crunching the numbers...'):
@@ -205,10 +99,30 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
-
+    if page_selection == "Solution Overview":
+        st.title("Solution Overview")
+        st.write("Describe your winning approach on this page")
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
+    
+    source = st.sidebar.checkbox("Data Source")
+    if source:
+        st.subheader("Where did we get the data from?")
+        st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/1200px-IMDB_Logo_2016.svg.png", use_column_width=False)
+        st.markdown("The data was obtained from the MovieLens which has the several millions 5-star ratings obtained from users using the online recommendation system. The IMBD (imbd_df) was legally scraped from IMDB.")
+    
+    
+    
+    
+    look = st.sidebar.checkbox("Data")
+    if look:
+        st.subheader("What did the data consist?")
+        st.markdown("* Movies - Movie titles are entered manually or imported from https://www.themoviedb.org/, and include the year of release in parentheses")
+        st.markdown("* Genome_scores - is a score mapping the strength between movies and tag-related properties.")
+        st.markdown("* Genome_tags - User assigned for the movies within the dataset.")
+        st.markdown("* Imdb - Additional movie metadata scraped from IMDB using the links.csv file.")
+    
 
     if page_selection == "Welcome":
         st.title("Welcome to STREAM-X")
@@ -218,8 +132,9 @@ def main():
 
         st.image('https://cdn.discordapp.com/attachments/1218114105395642428/1225045830063493130/16609E66-49C5-4B70-9315-30AB18D02A03.jpg?ex=661fb3d6&is=660d3ed6&hm=ab1c6ad50f8f8fdf9c64b1958a868d9fa9a463e25be90b54c8a1008e3bd09b65&', width=640)
 
-        st.write("The aim for this application is to give users the ability to discover new movies based on their 3 favourite movies.")
         st.info("Stream-X is a revolutionary movie recommendation algorithm that caters to the needs of visually impaired customers. By utilizing advanced technology, Stream-X is able to recommend up to 10 movies based on the individual preferences of each user. This algorithm takes into account various factors such as genre, actors, directors, and even user ratings to provide a personalized list of movie recommendations. What sets Stream-X apart from other recommendation algorithms is its accessibility features for visually impaired customers. Through the use of audio descriptions and voice commands, users can easily navigate through the recommended movies and select the ones they are interested in watching. This ensures that visually impaired customers can enjoy a seamless movie-watching experience without any barriers. Stream-X is constantly updating its database with the latest movies and user feedback to provide the most accurate and relevant recommendations. Whether you're in the mood for a comedy, drama, action, or horror movie, Stream-X has got you covered. With its user-friendly interface and innovative features, Stream-X is changing the way visually impaired customers discover and enjoy movies. Say goodbye to endless scrolling and indecision, and let Stream-X do the work for you.")
+    
+    
     if page_selection == "EDA":
         
         st.title("Exploratory Data Analysis (EDA)")
@@ -247,6 +162,8 @@ def main():
             elif i == 3:
                 st.write("This is a line graph which shows the Top 10 high budget movies of which My Way is the Top budget Movie.") 
     
+    
+            
     if page_selection == "About":
         st.title("Problem Statement")
         st.markdown("""
@@ -266,25 +183,26 @@ Companies like Netflix, Amazon Prime, and Disney leverage sophisticated algorith
         image_paths = ['resources/imgs/godfrey.jpg', 'resources/imgs/sethu.jpg', 'resources/imgs/mp.jpg', 'resources/imgs/confi.jpg', 'resources/imgs/onka.jpg', 'resources/imgs/ayanda.jpg' ]
         st.image(image_paths, caption=['Godfrey', 'Siphosethu', 'Mpho', 'Confidence', 'Onkarabile', 'Ayanda'], use_column_width=True, width=300)
     
-    source = st.sidebar.checkbox("Data Source")
-    if source:
-            st.subheader("Where did we get the data from?")
+        
+    if page_selection == "Your Feedback":
+        st.title("Feedback")
+        st.write("We would appreciate your ratings")
 
-            st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/1200px-IMDB_Logo_2016.svg.png", use_column_width=False)
+        # Create a slider for rating
+        rating = st.slider("Rate our service", min_value=1, max_value=5, step=1)
 
-            st.markdown("The data was obtained from the MovieLens \
-            which has the several millions 5-star ratings obtained from users using the online recommendation system.\
-            The IMBD (imbd_df) was legally scraped from IMDB.")
+        # Get feedback from the user
+        feedback = st.text_area("Please provide your feedback here:")
 
-    look = st.sidebar.checkbox("Data")
-    if look:
-            st.subheader("What did the data consists?")
-            st.markdown("* Movies -Movie titles are entered manually or imported from https://www.themoviedb.org/, and include the year of release in parentheses")
-            st.markdown("* Genome_scores - is a score mapping the strength between movies and tag-related properties.")
-            st.markdown("* Genome_tags - User assigned for the movies within the dataset.")
-            st.markdown("* Imdb - Additional movie metadata scraped from IMDB using the links.csv file.")
-            st.markdown("* Links - File providing a mapping between a MovieLens ID and associated IMDB and TMDB IDs.")
-            st.markdown("* Train - The training split of the dataset. Contains user and movie IDs with associated rating data.")    
+        # Save feedback to a file or database when submitted
+        if st.button("Submit Feedback"):
+            save_feedback(rating, feedback)
+            st.success("Thank you for your feedback!")
+
+def save_feedback(rating, feedback):
+    with open("feedback.txt", "a") as file:
+        file.write(f"Rating: {rating}, Feedback: {feedback}\n")
+ 
 
 
 if __name__ == '__main__':
